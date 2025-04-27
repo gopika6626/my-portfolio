a59c58b491a4e71a20a2b7d55a5a1eb4bfb218be
gopika6626 <kalagopika03@gmail.com>
2025-04-27 21:39:38 +0530

    latest

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
+import React from 'react';
 import styles from "./Projects.module.css";
 import projects from "../../data/projects.json";
 
 export const Projects = () => {
   return (
-    
-      <section className= {styles.container}>
-        <div className = {styles.content}>
-            <h3 className = {styles.title}>Projects</h3>
-            <div>
-                {
-                    projects.map((project,id) => {
-                        return (
-                            <div key = {id} className = {styles.projectCard}>
-                                <img className={styles.projectImage}
-                                src = {project.imageSrc}
-                                alt = {`image of ${project.title}`}/>
-                                <h3>{project.title}</h3>
-                                <p>{project.description}</p>
-
-                            </div>
-                        
-                        
-                    
-                     ) }
-
-                )
-                }
-                
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projectsContainer}>
+          {projects.map((project, id) => (
+            <div key={id} className={styles.flipCard}>
+              <div className={styles.flipCardInner}>
+                <div className={styles.flipCardFront}>
+                  <img 
+                    className={styles.projectImage}
+                    src={project.imageSrc}
+                    alt={`${project.title} project`}
+                  />
+                  <h3 className={styles.projectTitle}>{project.title}</h3>
+                </div>
+                <div className={styles.flipCardBack}>
+                  <div className={styles.projectDescription}>
+                    <h3>{project.title}</h3>
+                    <p>{project.description}</p>
+                    {project.technologies && (
+                      <div className={styles.technologies}>
+                        {project.technologies.map((tech, index) => (
+                          <span key={index} className={styles.techTag}>
+                            {tech}
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
+          ))}
         </div>
-        
-      </section>
-  
+      </div>
+    </section>
   );
 };
-
-
